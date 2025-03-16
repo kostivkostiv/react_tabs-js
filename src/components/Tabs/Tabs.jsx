5cc0e8ae114ee83b82a7af8fc159f715f57e7ef8
Maksym Kostiv <rafik9202@gmail.com>
2025-03-16 20:50:57 +0200

    code fix

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,24 +1,20 @@
 export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
   return (
     <div data-cy="TabsComponent">
       <div className="tabs is-boxed">
         <ul>
           {tabs.map(tab => (
             <li
-              className={tab.id === activeTabId ? 'is-active' : ''}
+              className={tab.id === activeTab.id ? 'is-active' : ''}
               data-cy="Tab"
               key={tab.id}
             >
               <a
                 href={`#${tab.id}`}
                 data-cy="TabLink"
-                onClick={event => {
-                  if (tab.id !== activeTabId) {
-                    onTabSelected(tab.id);
-                  } else {
-                    event.preventDefault();
-                  }
-                }}
+                onClick={() => tab.id !== activeTabId && onTabSelected(tab.id)}
               >
                 {tab.title}
               </a>
